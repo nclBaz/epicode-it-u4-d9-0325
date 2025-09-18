d5aaee79d54bee52d5be8c5eb84dfd8882f66e53
Riccardo Gulin <riccardo.gulin@gmail.com>
2025-09-18 11:58:39 +0200

    Adds comparators & limit examples

--- src/main/java/riccardogulin/Application.java
+++ src/main/java/riccardogulin/Application.java
@@ -35,7 +35,8 @@ public class Application {
 		usersByCity.forEach((city, usersList) -> System.out.println("Città: " + city + ", " + usersList));
 
 		// 2. Raggruppiamo gli utenti per età
-		Map<Integer, List<User>> usersByAge = users.stream().collect(Collectors.groupingBy(user -> user.getAge()));
+		Map<Integer, List<User>> usersByAge = users.stream()
+				.collect(Collectors.groupingBy(user -> user.getAge()));
 		usersByAge.forEach((age, usersList) -> System.out.println("Età: " + age + ", " + usersList));
 
 		// 3. Concateniamo tutti i nomi e cognomi degli utenti: "Aldo Baglio. Giovanni Storti. Giacomo Poretti. ...."
@@ -53,5 +54,51 @@ public class Application {
 		// 6. Calcolo la media delle età
 		double average = users.stream().collect(Collectors.averagingInt(user -> user.getAge()));
 		System.out.println("La media delle età è: " + average);
+
+		// 7. Raggruppiamo per città e calcoliamo la media delle età per ognuna di esse
+		Map<String, Double> averageAgePerCity = users.stream()
+				.collect(
+						Collectors.groupingBy(
+								user -> user.getCity(),
+								Collectors.averagingInt(user -> user.getAge())
+						)
+				);
+		averageAgePerCity.forEach((city, averageAge) -> System.out.println("Città: " + city + ", " + averageAge));
+
+		// 8. Raggruppiamo per città e calcoliamo tutta una serie di statistiche su quegli utenti, come media età, età massima, età minima...
+		Map<String, IntSummaryStatistics> statsPerCity = users.stream()
+				.collect(
+						Collectors.groupingBy(user -> user.getCity(),
+								Collectors.summarizingInt(user -> user.getAge()))
+				);
+		statsPerCity.forEach((city, stats) -> System.out.println("Città: " + city + ", " + stats));
+
+		// ********************************************************* COMPARATORS *********************************************************************
+		System.out.println("********************************************************* COMPARATORS *********************************************************************");
+		// I Comparators servono per stabilire un ordine di ordinamento dei dati quando si utilizza il metodo .sorted()
+
+		// 1. Ordiniamo gli utenti per età (Ordine Crescente)
+		List<User> sortedUsers = users.stream().sorted(Comparator.comparing(User::getAge)).toList();
+		sortedUsers.forEach(user -> System.out.println(user));
+
+		// 2. Ordiniamo gli utenti per età (Ordine Decrescente)
+		List<User> sortedUsersReverse = users.stream().sorted(Comparator.comparing(User::getAge).reversed()).toList();
+		// Se metto reversed non posso usare la lambda ma devo usare la dicitura con ::
+		sortedUsersReverse.forEach(user -> System.out.println(user));
+
+		// 3. Ordiniamo gli utenti per cognome
+		List<User> sortedUsersBySurname = users.stream().sorted(Comparator.comparing(user -> user.getSurname())).toList();
+		sortedUsersBySurname.forEach(user -> System.out.println(user));
+
+		// ********************************************************* LIMIT *********************************************************************
+		System.out.println("********************************************************* LIMIT *********************************************************************");
+
+		// 1. Ottengo la lista dei 5 users più vecchi
+		List<User> top5OldUsers = users.stream().sorted(Comparator.comparing(User::getAge).reversed()).limit(5).toList();
+		top5OldUsers.forEach(user -> System.out.println(user));
+
+		// 1. Ottengo la lista dei secondi 5 users più vecchi (dal sesto al decimo)
+		List<User> other5OldUsers = users.stream().sorted(Comparator.comparing(User::getAge).reversed()).skip(5).limit(5).toList();
+		other5OldUsers.forEach(user -> System.out.println(user));
 	}
 }
